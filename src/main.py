6e1b9945c8919d7837b4104947e41098a3d21e1c
Artemii Savoliuk <introductiontogit@gmail.com>
2026-08-31 15:04:38 +0200

    Collect the link to every book on page 1

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import requests
 from pathlib import Path
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 URL = "https://books.toscrape.com/"
 
@@ -9,22 +11,48 @@ HEADERS = {
     "User-Agent": "https://github.com/shiirotech/polite_scraper"
 }
 
-if CACHE_FILE.exists():
-    html = CACHE_FILE.read_text(encoding="utf-8")
 
-    print("CACHE HIT")
-    print(f"Size of the file: {len(html.encode("utf-8"))} bytes")
+def fetch(url: str, headers: dict, cache_file: Path) -> None:
+    if cache_file.exists():
+        html = cache_file.read_bytes()
 
-else:
-    response = requests.get(URL, headers=HEADERS, timeout=5)
+        print("CACHE HIT")
+        print(f"Size of the file: {len(html)} bytes")
 
-    if response.status_code != 200:
-        response.raise_for_status()
+    else:
+        response = requests.get(url, headers=headers, timeout=5)
 
-    html = response.text
+        if response.status_code != 200:
+            response.raise_for_status()
 
-    CACHE_FILE.parent.mkdir(parents=True, exist_ok=True)
-    CACHE_FILE.write_text(html, encoding="utf-8")
+        html = response.content
 
-    print("FETCH")
-    print(f"Size of the file: {len(response.content)} bytes")
+        cache_file.parent.mkdir(parents=True, exist_ok=True)
+        cache_file.write_bytes(html)
+
+        print("FETCH")
+        print(f"Size of the file: {len(html)} bytes")
+
+
+def parse(cache_file: Path) -> BeautifulSoup:
+    if cache_file.exists():
+        with open(cache_file, "r", encoding="utf-8") as f:
+            soup = BeautifulSoup(f.read(), "html.parser")
+        return soup
+    
+    else:
+        raise FileNotFoundError("File does not exist")
+
+
+if __name__ == "__main__":
+    fetch(URL, HEADERS, CACHE_FILE)
+    parsed = parse(CACHE_FILE)
+
+    book_links = set()
+    for link in parsed.select("article.product_pod h3 a"):
+        href = link.get("href")
+        absolute_url = urljoin(URL, href)
+        book_links.add(absolute_url)
+
+    for link in book_links:
+        print(link)
